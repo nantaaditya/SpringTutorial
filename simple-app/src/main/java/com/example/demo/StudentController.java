de0cab6e3e283edad6e71f8648a0ab0004d65861
TanoSutano <58139137+TanoSutano@users.noreply.github.com>
2020-03-05 10:35:49 +0700

    Add files via upload

--- simple-app/src/main/java/com/example/demo/StudentController.java
+++ simple-app/src/main/java/com/example/demo/StudentController.java
@@ -2,9 +2,12 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,4 +47,20 @@ public class StudentController {
   public Student getById(@PathVariable String id) {
     return studentService.getById(id);
   }
+  
+  @PutMapping(
+	      value = "/students/update/{id}",
+	      produces = MediaType.APPLICATION_JSON_VALUE
+	  )
+  public Student updateStudent(@PathVariable String id, @RequestBody Student updateStudent) {
+	return studentService.updateStudent(id, updateStudent);
+  }
+  
+  @DeleteMapping(
+		  value = "/students/delete/{id}"
+		 )
+  public Student deleteStudent(@PathVariable String id) {
+	  return studentService.deleteStudent(id);
+  }
+  
 }
