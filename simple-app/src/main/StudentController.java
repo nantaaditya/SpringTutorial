35a5ae1292fe09cab555a2b1f14b1fac2ff04333
TanoSutano <58139137+TanoSutano@users.noreply.github.com>
2020-03-05 11:24:43 +0700

    Add files via upload

---
+++ simple-app/src/main/StudentController.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * created by pramuditya.anantanur
+ **/
+@RestController
+public class StudentController {
+  
+  @Autowired
+  private StudentService studentService;
+  
+  @PostMapping(
+      value = "/students",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public Student add(@RequestBody Student student) {
+    studentService.add(student);
+    return student;
+  }
+  
+  @GetMapping(
+      value = "/students",
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public List<Student> findAll() {
+    return studentService.get();
+  }
+
+  @GetMapping(
+      value = "/students/{id}",
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public Student getById(@PathVariable String id) {
+    return studentService.getById(id);
+  }
+  
+  @PutMapping(
+	      value = "/students/update/{id}",
+	      produces = MediaType.APPLICATION_JSON_VALUE
+	  )
+  public Student updateStudent(@PathVariable String id, @RequestBody Student updateStudent) {
+	return studentService.updateStudent(id, updateStudent);
+  }
+  
+  @DeleteMapping(
+		  value = "/students/delete/{id}"
+		 )
+  public Student deleteStudent(@PathVariable String id) {
+	  return studentService.deleteStudent(id);
+  }
+  
+}
